b2c1c1f867923556c5f55bb79dce9139c91cc009
AntonioDMV <Antonio-DMV@outlook.com>
2022-12-17 16:31:58 -0500

    tarea-1

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import quotes from '../quotes.json'
+const QuoteBox = () => {
+
+    const random = Math.floor(Math.random() * quotes.length);
+    const [index, setIndex] = useState(random)
+
+    const changePhrase = () => {
+        const randomPhrase = Math.floor(Math.random() * quotes.length);
+        setIndex(randomPhrase)
+    }
+
+    const colors = [ '#638FBF', '#717DB6', '#8269A4', '#90548B', '#97406B', '#35B8CE', '#2ACFC6', '#66E3AC', '#ACF18B', '#F9F871'   ]
+    const randomColor = Math.floor(Math.random() * colors.length)
+    document.body.style = `background: ${colors[randomColor]}`
+    
+
+
+    return (
+        <div className='Pbox ' style={{ color: colors[randomColor] }}>
+            <div className='box'>
+                <p>{quotes[index].quote}</p>
+                <h3>{quotes[index].author}</h3>
+                <button style={{background: colors[randomColor]}} onClick={changePhrase}><i className="fa-solid fa-chevron-right"></i></button>
+            </div>
+        </div>
+    );
+};
+
+export default QuoteBox;
